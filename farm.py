f70d0934f86dcddb60dac4f57343a26295b0866a
2021sandeep <sandeep.j@thundersoft.com>
2022-09-28 16:27:38 +0530

    farm of lags

---
+++ farm.py
@@ -0,0 +1,10 @@
+
+# animal farm
+def animals(chickens,cows,pigs)
+total chickens=chickens*2
+total cows=cows*4
+total pigs=pigs*4
+return total chickens+totalcows+totalpigs
+
+print(animals(2,3,5)
+
