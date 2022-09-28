abc799b81d7943e38c81d271b05cdf18018d9f98
2021sandeep <sandeep.j@thundersoft.com>
2022-09-28 15:37:53 +0530

    my new add commit

--- rotationlist.py
+++ rotationlist.py
@@ -2,3 +2,17 @@
 mylist=[20,15,26,8,4]
 mylist[0],mylist[-2]=mylist[-2],mylist[0]
 print("list after swaping:",mylist)
+
+#rotation of a list method 2
+list=[]
+num=int(input("enter the number insert in list \n")
+        for i in range(1,num+1)
+            print("enter",i,"elements")
+            list.append(int(input()))
+        print("list before swaping \n",list)
+        lenth=len(list)
+        list[0],list[length-1]=list[length-1],list[0]
+        print("\n list after swapping\n",list)
+
+
+
